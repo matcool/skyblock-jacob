385dfc4d7c298fc99e7d19f75ba2a1508e5114ee
matcool <26722564+matcool@users.noreply.github.com>
2020-11-22 14:53:21 -0300

    oops

--- client/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from 'vue';
-import type { Note } from './types';
-
-export const notes = reactive([
-    {
-        title: 'hello',
-        content: 'welcome to my blog',
-    },
-] as Note[]);
